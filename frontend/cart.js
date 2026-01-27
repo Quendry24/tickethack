54416152866e7589b91cdb72c92b5735de3b9d35
Quentin~ <quendry@hotmail.fr>
2026-01-27 18:15:37 +0100

    fin de journée frontend

--- frontend/cart.js
+++ frontend/cart.js
@@ -1,9 +1,34 @@
-document.querySelector('#logo').addEventListener('click', () => {
-    window.location.assign('/index.html')
-})
-document.querySelector('#cart').addEventListener('click', () => {
-    window.location.assign('/cart.html')
-})
-document.querySelector('#booking').addEventListener('click', () => {
-    window.location.assign('/booking.html')
-})
+fetch('http://localhost:3000/voyages').then(res => res.json()).then(data => console.log(data))
+
+
+
+// fetch('http://localhost:3000/voyages/addtocart', {
+//     method:'POST',
+//     headers: {'Type-Content' : 'application/json',
+//         body: JSON.stringify({ voyageId })
+//     }
+// }).then(res => res.json()).then(data => {
+//     if (data.message === "Remplissez Departure, Arrival et Date !") {
+//         document.querySelector('#returnCard').innerHTML = `
+//             <img src=".\images\train.png" alt="train" id="icon">
+//                 <p>It's time to book your future trip</p>
+//             `
+//         document.querySelector('#icon').src = './images/notfound.png'
+//         document.querySelector('#returnCard>p').textContent = "No trip found"
+//     }
+//     else {
+//         document.querySelector('#returnCard').innerHTML = "";
+//         for (let trajet of data) {
+//             document.querySelector('#returnCard').innerHTML += `
+//                 <div class="trajet">
+//                     <p>${trajet.departure}>${trajet.arrival}</p>
+//                     <p>${trajet.hour}</p>
+//                     <p><strong>${trajet.price}€</strong></p>
+//                     <button class="bookBtn">Book</button>
+//                 </div>
+//                 `
+//         }
+//         document.querySelector('#departureCity').value = ''
+//         document.querySelector('#arrivalCity').value = ''
+//     }
+// })
